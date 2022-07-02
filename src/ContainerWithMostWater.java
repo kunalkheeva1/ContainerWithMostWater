ac1a08b1527f3669af32a611e71bf2b3a0f23a4d
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-01 21:48:45 -0700

    created a method that returns the maximum area that bars can obtain, with the help of heights as input, and for this problem the width increments by the indexes.

--- src/ContainerWithMostWater.java
+++ src/ContainerWithMostWater.java
@@ -1,2 +1,27 @@
 public class ContainerWithMostWater {
+
+
+    //int array[] contains the heights of the bars
+
+    public static int ContainerWithMostWater(int arr[]){
+        int i= 0;
+        int j= arr.length;
+        int maxArea = 0;
+        while (i<j){
+            int height = Math.min(arr[i], arr[j]);
+            int width = j-i;
+            int area = height * width;
+            maxArea = Math.max(maxArea, area);
+            if(arr[i]< arr[j]){
+                i++;
+            }else{
+                j--;
+            }
+        }return maxArea;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
